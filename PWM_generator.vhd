38da7673ef29805f3e95b329c18a34d92755b32c
AlbericSimperl1 <alberic.simperl@student.kuleuven.be>
2025-11-16 19:50:44 +0100

    english

--- PWM_generator.vhd
+++ PWM_generator.vhd
@@ -6,16 +6,25 @@ entity pwm is
     port (
         clk : in std_logic;
         rst : in std_logic;
-        -- pwm waarden
-        snelheid : in unsigned(7 downto 0);
-        richting : in std_logic;
-        -- singaal
+        -- pwm variables
+        v : in unsigned(7 downto 0); -- velocity
+        dir : in std_logic; -- direction: 0 forwards, 1 backwards
+        -- output
         pwm_out : out std_logic
     );
 end pwm;
 
 architecture rtl of pwm is
+    signal ctr : unsigned(7 downto 0);
 
 begin
+    process(clk)
+    begin
+        if rising_edge(clk) then
+            if rst = '1' then
+                ctr <= (others => '0');
+                pwm_out <= '0';
+            end if;
+        end if;
 
 end architecture;
