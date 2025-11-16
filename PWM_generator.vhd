388eb96d95478cb2414097194bf4c8b320ea8811
AlbericSimperl1 <alberic.simperl@student.kuleuven.be>
2025-11-16 19:58:33 +0100

    english and architecture

--- PWM_generator.vhd
+++ PWM_generator.vhd
@@ -4,27 +4,35 @@ use ieee.numeric_std.all;
 
 entity pwm is
     port (
+        -- standard
         clk : in std_logic;
         rst : in std_logic;
         -- pwm variables
-        v : in unsigned(7 downto 0); -- velocity
+        vel : in unsigned(7 downto 0); -- velocity
         dir : in std_logic; -- direction: 0 forwards, 1 backwards
         -- output
         pwm_out : out std_logic
     );
 end pwm;
 
-architecture rtl of pwm is
+architecture pwm_architecture of pwm is
     signal ctr : unsigned(7 downto 0);
 
 begin
     process(clk)
     begin
         if rising_edge(clk) then
-            if rst = '1' then
+            if rst = '1' then -- rst resets
                 ctr <= (others => '0');
                 pwm_out <= '0';
+            else
+                ctr <= ctr + 1;
+                if ctr < vel then
+                    pwm_out <= '1';
+                else
+                    pwm_out <= '0';
+                end if;
             end if;
         end if;
-
+    end process;
 end architecture;
