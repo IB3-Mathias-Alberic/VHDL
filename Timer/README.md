4fc24151394714beb04f842c2d44a96d135a48e2
AlbericSimperl1 <alberic.simperl@student.kuleuven.be>
2025-11-24 14:47:12 +0100

    timer readme

---
+++ Timer/README.md
@@ -0,0 +1,3 @@
+## Timer
+- count how long we need to keep doing a command
+- 1 bit wide input: app mode (live/array)
--- Timer/timer.vhd
+++ Timer/timer.vhd
@@ -1,1 +1,20 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
+entity ent is
+    port (
+        -- standard
+        clk : in std_logic;
+        rst : in std_logic;
+        -- time
+        
+
+    );
+end ent;
+
+architecture rtl of ent is
+
+begin
+
+end architecture;
