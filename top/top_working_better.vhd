8e073ef52238206205b7cd110748dba870fc57c3
AlbericSimperl1 <alberic.simperl@student.kuleuven.be>
2025-12-17 10:38:03 +0100

    delta

--- top/top_working_better.vhd
+++ top/top_working_better.vhd
@@ -140,9 +140,6 @@ begin
     dir_2 <= (d_2_top xor dir_M) & ((not d_2_top) xor dir_M);
     dir_3 <= (d_3_top xor dir_M) & ((not d_3_top) xor dir_M);
     dir_4 <= (d_4_top xor dir_M) & ((not d_4_top) xor dir_M);
-
-   
-
     
     -- Gate PWM outputs with enable signals
     ena_front <= ena_1_top and ena_2_top;
